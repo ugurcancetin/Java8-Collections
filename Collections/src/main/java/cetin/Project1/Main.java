8db855ae516e96f985491b9db15e280411148e7a
ugurcancetin <cetinyazilimlar@gmail.com>
2019-10-24 01:08:19 +0200

    Shuffle and sort with buble sort algorithm on a simple way

--- Collections/src/main/java/cetin/Project1/Main.java
+++ Collections/src/main/java/cetin/Project1/Main.java
@@ -1,19 +1,27 @@
 package cetin.Project1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        theatre.getSeats();
-        if(theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
-        if(theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
+        List<Theatre.Seat> l = new ArrayList<>(theatre.seats);
+        Collections.shuffle(l);
+        l.forEach((i) -> System.out.println(i.getSeatNumber()));
+        sortList(l);
+        l.forEach((i) -> System.out.println(i.getSeatNumber()));
+
     }
 
+    private static void sortList(List<? extends Theatre.Seat> list){
+        for(int i=0; i<list.size()-1; i++){
+            for(int j=i+1; j<list.size(); j++){
+                if(list.get(i).compareTo(list.get(j)) > 0){
+                    Collections.swap(list, i , j);
+                }
+            }
+        }
+    }
 }
